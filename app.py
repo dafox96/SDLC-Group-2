50b204ac3a3dc54bec9ca257ff0dd1ca397ae907
mmoga <matt.moga@gmail.com>
2024-03-25 17:32:59 -0400

    Cleaned up file structure

--- app.py
+++ app.py
@@ -1,51 +1,40 @@
 from flask import Flask, request, render_template, redirect, url_for
+from os import getenv
+from flask_bcrypt import Bcrypt
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+from flask_login import (
+    UserMixin,
+    login_user,
+    LoginManager,
+    current_user,
+    logout_user,
+    login_required,
+)
+
+login_manager = LoginManager()
+login_manager.session_protection = "strong"
+login_manager.login_view = "login"
+login_manager.login_message_category = "info"
+
+db = SQLAlchemy()
+bcrypt = Bcrypt()
+migrate = Migrate()
+
 
 # Flask constructor
-app = Flask(__name__)
+def create_app():
+    app = Flask(__name__)
 
+    app.secret_key = getenv("LOGIN_SECRET")
+    app.config["SQLALCHEMY_DATABASE_URI"] = (
+        "sqlite:///site.db"  # Using SQLite as the database
+    )
 
-@app.route("/")
-def home():
-    return render_template("index.html")
+    login_manager.init_app(app)
+    db.init_app(app)
+    migrate.init_app(app, db)
+    bcrypt.init_app(app)
 
-
-@app.route("/login")
-def login_page():
-    return render_template("login.html")
-
-
-@app.route("/add-game")
-def add_games():
-    return render_template("add-game.html")
-
-
-# Route for handling the login page logic
-@app.route("/test", methods=["GET", "POST"])
-def login():
-    error = None
-    if request.method == "POST":
-        if request.form["username"] != "admin" or request.form["password"] != "admin":
-            error = "Invalid Credentials. Please try again."
-        else:
-            return redirect(url_for("home"))
-    return render_template("login.html", error=error)
-
-
-@app.route("/library")
-def library():
-    return render_template("library.html")
-
-
-# A decorator used to tell the application
-# which URL is associated function
-# @app.route("/test.py", methods=["GET", "POST"])
-# def get_freq():
-#     if request.method == "POST":
-#         # getting input with freq = set_freq in HTML form
-#         freq = request.form.get("set_freq")  # <--- do whatever you want with that value
-#         return "Your freq value is " + freq
-#     return render_template("add-game.html")
-
-
-if __name__ == "__main__":
-    app.run()
+    return app
---
+++ forms.py
@@ -0,0 +1,47 @@
+from wtforms import (
+    StringField,
+    PasswordField,
+    BooleanField,
+    IntegerField,
+    DateField,
+    TextAreaField,
+)
+
+from flask_wtf import FlaskForm
+from wtforms.validators import InputRequired, Length, EqualTo, Email, Regexp, Optional
+from flask_login import current_user
+from wtforms import ValidationError, validators
+from models import User
+
+
+class login_form(FlaskForm):
+    pwd = PasswordField(validators=[InputRequired(), Length(min=8, max=72)])
+    # Placeholder labels to enable form rendering
+    username = StringField(validators=[Optional()])
+
+
+class register_form(FlaskForm):
+    username = StringField(
+        validators=[
+            InputRequired(),
+            Length(3, 20, message="Please provide a valid name"),
+            Regexp(
+                "^[A-Za-z][A-Za-z0-9_.]*$",
+                0,
+                "Usernames must have only letters, " "numbers, dots or underscores",
+            ),
+        ]
+    )
+
+    pwd = PasswordField(validators=[InputRequired(), Length(8, 72)])
+    cpwd = PasswordField(
+        validators=[
+            InputRequired(),
+            Length(8, 72),
+            EqualTo("pwd", message="Passwords must match!"),
+        ]
+    )
+
+    def validate_uname(self, username):
+        if User.query.filter_by(username=username.data).first():
+            raise ValidationError("Username already taken!")
